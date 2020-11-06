f8db2bdb84de4f2412684a34ca626bf5ac3e86a3
Rodrigo Bossini <professorbossini@gmail.com>
2020-11-05 21:28:27 -0300

    Terminamos o pdf 13. Retomar estilizando o cabecalho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { ClienteInserirComponent } from './clientes/cliente-inserir/cliente-inserir.component';
@@ -24,6 +25,7 @@ import { ClienteListaComponent } from './clientes/cliente-lista/cliente-lista.co
     ClienteListaComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
